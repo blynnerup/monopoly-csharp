503b39cd8f6da3298c6e070bfd523578368017ad
Brian Lynnerup Pedersen <brian.lynnerup.pedersen@gmail.com>
2024-04-05 13:26:17 +0200

    Basic setup of project complete.
    Can begin timing and coding

---
+++ MonopolyCSharp/MonopolyCSharp/GameLogic.cs
@@ -0,0 +1,10 @@
+namespace MonopolyCSharp;
+
+public class GameLogic
+{
+    private static void Main(string[] args)
+    {
+        // Display the number of command line arguments.
+        Console.WriteLine(args.Length);
+    }
+}
