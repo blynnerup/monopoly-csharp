e997a72193f79d9c7a543f3b35ef5aea2f33789a
Brian Lynnerup Pedersen <brian.lynnerup.pedersen@gmail.com>
2024-04-15 06:58:29 +0200

    Changes to the test data and handle of json

--- MonopolyCSharp/MonopolyCSharp/GameBoard.cs
+++ MonopolyCSharp/MonopolyCSharp/GameBoard.cs
@@ -4,23 +4,19 @@ namespace MonopolyCSharp;
 
 public class GameBoard
 {
-    public List<GameBoardField>? Fields { get; set; }
+    public List<GameBoardField>? Fields { get; set; } = [];
     public List<ChanceCard>? ChanceCards { get; set; }
 
-    public GameBoard()
-    {
-        Fields = new List<GameBoardField>();
-
-    }
     public void InitializeBoard()
     {
         var options = new JsonSerializerOptions
         {
             PropertyNameCaseInsensitive = true
         };
-        var jsonString = File.ReadAllText("StaticGameData/gameFields.json");
+        var jsonString = File.ReadAllText("StaticGameData/testGameField.json");
+        // var jsonString = File.ReadAllText("StaticGameData/gameFields.json");
         // var field = JsonSerializer.Deserialize<GameBoardField>(jsonString, options);
-        Fields = JsonSerializer.Deserialize<List<GameBoardField>>(jsonString)!;
+        Fields = JsonSerializer.Deserialize<List<GameBoardField>>(jsonString, options)!;
     }
 
     private void InitializeChanceCards()
--- MonopolyCSharp/MonopolyCSharp/GameBoardField.cs
+++ MonopolyCSharp/MonopolyCSharp/GameBoardField.cs
@@ -1,28 +1,31 @@
+using System.Runtime.Serialization;
+
 namespace MonopolyCSharp;
 
 public class GameBoardField
 {
-    public string Name { get; set; } = null!;
-    public string FieldNumber { get; set; }
+    public string Text { get; set; } = null!;
+    public string FieldNumber { get; set; } = null!;
     public string Type { get; set; } = null!;
     public string? Price { get; set; }
     public string? Group { get; set; }
-    public Special? Special { get; set; }
-
+    public Special Special { get; set; } = new();
 }
 
 public class Special
 {
-    public List<Effect> Effects { get; set; } = null!;
+    public Effect Effect { get; set; }
+    public string? Amount { get; set; }
 }
 
 public enum Effect
 {
+    None,
     Chance,
     SubBalance,
     Balance,
     Jailed,
-    None,
     Visiting,
-    Choice
+    Choice,
+    Parking
 }
