e8c883c477a4b1b6dba516579f7ea43284df766b
Brian Lynnerup Pedersen <brian.lynnerup.pedersen@gmail.com>
2024-04-22 09:37:03 +0200

    Added chance cards

--- MonopolyCSharp/MonopolyCSharp/GameBoardField.cs
+++ MonopolyCSharp/MonopolyCSharp/GameBoardField.cs
@@ -20,12 +20,15 @@ public class Special
 
 public enum Effect
 {
-    None = 0,
-    Chance = 1,
-    SubBalance = 2,
-    Balance = 3,
-    Jailed = 4,
-    Visiting = 5,
-    Choice = 6,
-    Parking = 7
+    None = 0, // No effect on card
+    Chance = 1, // Draw a card
+    SubBalance = 2, // Subtract from balance
+    Balance = 3, // Add to balance
+    Jailed = 4, // Jailed for x turns
+    Visiting = 5, // Jail
+    Choice = 6, // Cards or fields with a choice
+    Parking = 7, // Redundant?
+    Move = 8, // Move to a field (game field Id)
+    Tax = 9, // Chance card tax (could also be used for game field choice?
+    Collect = 10 // From other players
 }
